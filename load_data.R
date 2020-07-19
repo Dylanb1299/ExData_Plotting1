848de6a2ee76486b17c7dc71bd6f71d1bf4a911f
Dylanb1299 <dgrb3@cam.ac.uk>
2020-07-19 18:38:20 +0100

    Removed unnecessary clutter in load_data and plot1

--- load_data.R
+++ load_data.R
@@ -1,4 +1,6 @@
 library(graphics)
+library(dplyr)
+library(lubridate)
 
 if (!dir.exists("./data")) {
         dir.create("./data")
@@ -6,3 +8,8 @@ if (!dir.exists("./data")) {
 
 #load the data
 electricity <- read.table("./data/household_power_consumption.txt", header = TRUE, sep = ";")
+
+#select only the dates we care about
+dates <- dmy(electricity$Date)
+electricity <- electricity[dates >= ymd("2007-02-01") & dates <= ymd("2007-02-02"), ]
+
